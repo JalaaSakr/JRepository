d0f770b0897d7ec1324148b71159bb721958a123
JalaaSakr <119867222+JalaaSakr@users.noreply.github.com>
2022-12-05 19:44:09 +0300

    Add files via upload

---
+++ CatalogContext/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using CatalogLastApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductCatalogApp.CatalogContext
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+      
+
+    }
+}
